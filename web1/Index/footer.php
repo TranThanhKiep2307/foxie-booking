eafa934a8f8f144af93132185a198ed60a08daa2
TranThanhKiep2307 <ttkcaptianlp23@gmail.com>
2023-04-14 19:26:51 +0700

    kiep

---
+++ web1/Index/footer.php
@@ -0,0 +1,314 @@
+<!--footer-->
+<footer class="py-sm-5 py-4 px-md-5 px-3">
+	<div class="container-fluid pt-lg-5">
+		<div class="row">
+			<div class="col-lg-3 col-sm-6 mb-lg-0 mb-5 footer-grid-agileits-w3ls1 text-left">
+				<h3 class="mb-sm-5 mb-4 mt-sm-0 mt-4">Về ứng dụng của chúng tôi</h3>	
+				<p>Trang web của chúng tôi sẽ cung cấp cho bạn những chuyến bay an toàn và chất lượng nhất</p>
+				<a href="#" class="read">Xem thêm <i class="fas fa-caret-right"></i></a>
+			</div>
+			<div class="col-lg-3 col-sm-6 mb-lg-0 mb-5 footer-grid-agileits-w3ls1 text-left">
+				<h3 class="mb-sm-5 mb-4">Thông tin của chúng tôi</h3>	
+				<ul class="w3ls-footer-bottom-list">
+					<li> <span class="fas fa-map-marker"></span> Phát triển ứng dụng website<span> Cần Thơ </span></li>
+					<li> <span class="fas fa-envelope"></span> <a href="mailto:nhom08@student.ctu.edu.vn"> nhom08@student.ctu.edu.vn</a> </li>
+					<li> <span class="fas fa-phone"></span> +0123 456 789 </li>
+					<li> <span class="fas fa-fax"></span> +0123 456 789 </li>
+					<li> <span class="fas fa-globe"></span> <a href="#"> www.foxiebooking.com</a> </li>
+				</ul>
+			</div>
+			<div class="col-lg-3 col-sm-6 mb-sm-0 mb-5 footer-grid-agileits-w3ls text-left">
+				<div class="tech-btm">
+					<h3 class="mb-sm-5 mb-4">Tin mới nhất</h3>
+					<div class="blog-grids row mb-3">
+						<div class="col-4 pr-0 blog-grid-left">
+							<a href="#">
+								<img src="images/blog1.jpg" class="img-fluid" alt="">
+							</a>
+						</div>
+						<div class="col-8 blog-grid-right">
+
+							<h5>
+								<a href="#">Giảm giá sốc </a>
+							</h5>
+							<div class="sub-meta">
+								<span>
+									<i class="far fa-clock"></i> 01/04/2023</span>
+							</div>
+						</div>
+						
+					</div>
+					<div class="blog-grids row mb-3">
+						<div class="col-4 pr-0 blog-grid-left">
+							<a href="#">
+								<img src="images/blog2.jpg" class="img-fluid" alt="">
+							</a>
+						</div>
+						<div class="col-8 blog-grid-right">
+
+							<h5>
+								<a href="#">Khách sạn giá rẻ </a>
+							</h5>
+							<div class="sub-meta">
+								<span>
+									<i class="far fa-clock"></i> 02/04/2023</span>
+							</div>
+						</div>
+						
+					</div>
+					<div class="blog-grids row">
+						<div class="col-4 pr-0 blog-grid-left">
+							<a href="#">
+								<img src="images/blog3.jpg" class="img-fluid" alt="">
+							</a>
+						</div>
+						<div class="col-8 blog-grid-right">
+
+							<h5>
+								<a href="#">Vân chuyển hàng hóa </a>
+							</h5>
+							<div class="sub-meta">
+								<span>
+									<i class="far fa-clock"></i> 03/04/2023</span>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<!-- subscribe -->
+			<div class="col-lg-3 col-sm-6 subscribe-main footer-grid-agileits-w3ls text-left">
+				<h3 class="mb-sm-5 mb-4">Thắc mắc</h3>
+				<div class="subscribe-main text-left">
+					<div class="subscribe-form">
+						<form action="#" method="post" class="subscribe_form">
+							<input class="form-control" type="email" placeholder="Nhập Email của bạn..." required="">
+							<button type="submit" class="btn1 btn-primary submit"><i class="fas fa-paper-plane" aria-hidden="true"></i></button>
+						</form>
+				   </div>
+				</div>
+				<!-- //subscribe -->
+				<div class="footer-social">
+				<div class="copyrighttop">
+				<h3 class="mb-sm-5 mb-4">Liên hệ với chúng tôi qua</h3>
+					<ul>
+						<li class="mr-1">
+							<a class="facebook" href="#">
+								<i class="fab fa-facebook-f"></i>
+							</a>
+						</li>
+						<li class="mr-1">
+							<a class="facebook" href="#">
+								<i class="fab fa-twitter"></i>
+							</a>
+						</li>
+						<li class="mr-1">
+							<a class="facebook" href="#">
+								<i class="fab fa-google-plus-g"></i>
+							</a>
+						</li>
+						<li class="mr-1">
+							<a class="facebook" href="#">
+								<i class="fab fa-pinterest-p"></i>
+							</a>
+						</li>
+						<li>
+							<a class="facebook" href="#">
+								<i class="fab fa-linkedin-in"></i>
+							</a>
+						</li>
+					</ul>
+
+				</div>
+			</div>
+			</div>
+		</div>
+		<!-- copyright -->
+		<div class="footer-cpy text-center pt-sm-5 mt-sm-5 mt-4 pt-3">
+			<div class="w3layouts-agile-copyrightbottom">
+				<p>© 2023 Foixe Booking | Thiết kế bởi
+					<a href="#">Nhom8</a>
+				</p>
+
+			</div>
+		</div>
+		<!-- //copyright -->
+	</div>
+</footer>
+<!-- //footer -->
+<!-- login and register modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Đăng nhập</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form action="#" method="post">
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label">Tên người dùng</label>
+						<input type="text" class="form-control" placeholder="User Name" name="Name" id="recipient-name" required="">
+					</div>
+					<div class="form-group">
+						<label for="password" class="col-form-label">Mật khẩu</label>
+						<input type="password" class="form-control" placeholder="Password" name="Password" id="password" required="">
+					</div>
+					<div class="row sub-w3l my-3">
+						<div class="col sub-agile">
+							<input type="checkbox" id="brand1" value="">
+							<label for="brand1" class="text-white">
+								<span></span>Nhớ tôi?</label>
+						</div>
+						<div class="col forgot-w3l text-right">
+							<a href="#" class="text-white">Quên mật khẩu?</a>
+						</div>
+					</div>
+					<div class="right-w3l">
+						<input type="submit" class="form-control" value="Sign In">
+					</div>
+					<p class="text-center dont-do text-white mt-3">Bạn chưa có tài khoản?
+						<a href="#" data-toggle="modal" data-target="#exampleModal1" class="text-white">
+							Đăng ký ngay</a>
+					</p>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel1">Đăng ký</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form action="#" method="post">
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label">Tên người dùng</label>
+						<input type="text" class="form-control" placeholder="User Name" name="Name" id="recipient-rname" required="">
+					</div>
+					<div class="form-group">
+						<label for="recipient-email" class="col-form-label">Email</label>
+						<input type="email" class="form-control" placeholder="Email" name="Email" id="recipient-email" required="">
+					</div>
+					<div class="form-group">
+						<label for="password1" class="col-form-label">Mật khẩu</label>
+						<input type="password" class="form-control" placeholder="Password" name="Password" id="password1" required="">
+					</div>
+					<div class="form-group">
+						<label for="password2" class="col-form-label">Xác nhận lại mật khẩu</label>
+						<input type="password" class="form-control" placeholder="Confirm Password" name="Confirm Password" id="password2" required="">
+					</div>
+					<div class="sub-w3l">
+						<div class="sub-agile">
+							<input type="checkbox" id="brand2" value="">
+							<label for="brand2" class="mb-3">
+								<span></span>Tôi chấp nhận các điều khoản & điều kiện</label>
+						</div>
+					</div>
+					<div class="right-w3l">
+						<input type="submit" class="form-control" value="Register">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //login and register modal -->
+<!-- js-scripts -->		
+
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+	<!-- //js -->
+	
+	<!-- carousel -->
+	<script src="js/owl.carousel.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('.owl-carousel').owlCarousel({
+				loop: true,
+				margin: 10,
+				responsiveClass: true,
+				responsive: {
+					0: {
+						items: 1,
+						nav: true
+					},
+					600: {
+						items: 1,
+						nav: false
+					},
+					900: {
+						items: 2,
+						nav: false
+					},
+					1000: {
+						items: 3,
+						nav: true,
+						loop: false,
+						margin: 20
+					}
+				}
+			})
+		})
+	</script>
+	<!-- //carousel -->
+
+	<!--slider-->
+	<script src="js/responsiveslides.min.js"></script>
+	<script>
+		$(function () {
+			// Slideshow 1
+			$("#slider1").responsiveSlides({
+				auto: true,
+				pager: true,
+				nav: true,
+				speed: 500,
+				namespace: "centered-btns"
+			});
+		});
+	</script>
+	<!--//slider-->
+
+	<!-- start-smoth-scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+	<!-- start-smoth-scrolling -->
+	
+<!-- //js-scripts -->
+
+</body>
+</html>
---
+++ web1/Index/header.php
@@ -0,0 +1,103 @@
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Foxie Booking</title>
+	
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Taxi Cab Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+
+	<link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all">
+	<link rel="stylesheet" href="css/owl.theme.css" type="text/css" media="all">
+	<!-- css files -->
+	<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+	<link rel="stylesheet" href="css/fontawesome-all.css"> <!-- Font-Awesome-Icons-CSS -->
+	<!-- //css files -->
+
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+	<!-- //web-fonts -->
+	
+</head>
+
+<body>
+
+<!--/banner-->
+	<div class="top-bar_sub_w3layouts container-fluid">
+		<div class="row">
+			
+			<div class="col-md-4 col-sm-6 log-icons mt-2">
+				<p class="py-2"><i class="fas fa-phone"></i> Call: (0) 123 456 789</p>
+			</div>
+			
+			<div class="col-md-4 col-sm-6 logo">
+				<a class="navbar-brand" href="index.php">
+					<i class="fas fa-plane"></i> Foxie Booking</a>
+			</div>
+			
+			
+			<div class="col-md-4 top-forms mt-md-3 mt-2 mb-md-0 mb-3">
+				<span class="mx-lg-4 mx-md-2  mx-1">
+					<a href="#" data-toggle="modal" aria-pressed="false" data-target="#exampleModal">
+						<i class="fas fa-lock"></i> Đăng nhập</a>
+				</span>
+				<span>
+					<a href="#" data-toggle="modal" data-target="#exampleModal1" >
+						<i class="fas fa-user"></i> Đăng ký</a>
+				</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="banner" id="home">
+		<!-- header -->
+    </header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light top-header">
+				<button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+				    aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mx-auto">
+						<li class="nav-item">
+							<a class="nav-link ml-lg-0" href="index.php">Trang chủ
+								<span class="sr-only">(current)</span>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="about.php">Về chúng tôi</a>
+						</li>
+						
+						<li class="nav-item">
+							<a class="nav-link" href="booking.php">Đặt vé</a>
+						</li>
+						
+						<li class="nav-item active">
+							<a class="nav-link" href="contact.php">Liên hệ</a>
+						</li>
+					</ul>
+
+				</div>
+			</nav>
+		</header>
+		<!-- //header -->
